1ccd1ad494fc6b092fbc718753e6c89d6b8e0ca2
oindil <oindilgolder303@gmail.com>
2023-08-03 12:04:43 +0530

    added try catch block and refactored some code

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,24 +2,31 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const {body, validationResult} = require('express-validator')
-router.post('/',[ // here all the validations will be added
+// A user is created here. No login required. POST 'api/auth/createuser'
+router.post('/createuser',[ // here all the validations should be added
     body('name','Please enter a name of minimum 3 characters').isLength({min:3}),
     body('email','Please enter a valid email address').isEmail(),
     body('password','Please enter a password of minimum 8 characters').isLength({min:8}),
       
-],(req,res)=>{
+], async (req,res)=>{
+    // If there are errors return with bad requests and the errors
     const errors = validationResult(req)
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array()})
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }).then(user => res.json(user)).catch(err => {
-        console.log(err)
-        res.json({error:'Please enter a unique email'})
-    })
-    // res.send(req.body)
+    try{
+        let user = await User.findOne({email: req.body.email}) // this line will look for redundant email addresses
+        if(user){
+            return res.status(400).json({error: 'Sorry this email already exists'})
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.json(user)
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
 })
 module.exports = router
