a9041559632f8ed37b4faf2041107b1d620af6f2
oindil <oindilgolder303@gmail.com>
2023-09-12 07:10:20 +0530

    added the dark mode functionality to navbar

--- src/App.js
+++ src/App.js
@@ -6,12 +6,27 @@ import {
   Route,
 } from 'react-router-dom';
 import About from './Components/About';
+import { useState } from 'react';
 function App() {
+  const [mode, setMode] = useState('light');
+
+  function togglefunc(){
+    if(mode === 'light'){
+      setMode("dark")
+      document.body.style.backgroundColor = 'rgb(32, 33, 36)' 
+      document.body.style.color = 'white'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white' 
+      document.body.style.color = 'black'
+    }
+  }
   return (
     <>
       <div>
         <Router>
-        <Navbar/>
+        <Navbar mode={mode} togglefunc={togglefunc}/>
           <Routes>
             <Route exact path='/about' element={<About/>}></Route>
           </Routes>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,40 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const Navbar = () => {
+const Navbar = (props) => {
+  // React.useEffect(() => { // not working till now
+  //   console.log(location)
+  // }, [location]);
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className={`navbar navbar-expand-lg text-${props.mode === 'dark' ? 'light' : 'dark'} bg-${props.mode}`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            My Notes
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarText"
-            aria-controls="navbarText"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link " aria-current="page" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/"></Link>
-              </li>
-            </ul>
+          <Link className={`text-${props.mode === 'dark' ? 'light' : 'dark'} navbar-brand`} to='/'>MyNotes.</Link>
+          <div className="form-check-reverse form-check form-switch" >
+            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.togglefunc} />
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault"><i className="ri-contrast-2-line" ></i></label>
           </div>
         </div>
       </nav>
